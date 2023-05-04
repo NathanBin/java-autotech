aede8f9bdacf31dbc31e62698de7ad91cf8a2c3f
NathanBin <nathan.lopes@sptech.school>
2023-05-03 21:05:03 -0300

    nova versão script clean.sh

--- clean/clean.sh
+++ clean/clean.sh
@@ -1,15 +1,13 @@
-echo "Exibindo o consumo da memória RAM"
-free -h
-echo "Limpando Cache da Memória RAM"
-echo 3 > /proc/sys/vm/drop_caches
-sysctl -w vm.drop_caches=3
+echo "Limpando Cache do Sistema"
+sudo bleachbit -c system.cache
 clear
-echo "Limpeza do Cache da Memória RAM efetuada com sucesso"
-echo "Exibindo o consumo da mamória RAM"
-free -h
+echo "Limpando a Swap e liberando Memória"
+sudo bleachbit -c system.memory
+clear
 
 sudo apt-get autoclean
 sudo apt-get autoremove
 sudo apt-get clean
-sudo bleachbit -c apt.package_lists
 
+echo "Exibindo o consumo da mamória RAM"
+free -h
